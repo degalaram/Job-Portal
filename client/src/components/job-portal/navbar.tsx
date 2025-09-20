2c923733ea2494f90d40d19706617ad2e42f9692
Ram <136690243+degalaram@users.noreply.github.com>
2025-09-20 13:28:42 +0530

    Update navbar.tsx

--- client/src/components/job-portal/navbar.tsx
+++ client/src/components/job-portal/navbar.tsx
@@ -211,12 +211,7 @@ export function Navbar() {
               >
                 My Applications
               </a>
-              <a
-                href="/deleted-posts"
-                className="block px-3 py-2 text-base font-medium text-gray-700 dark:text-gray-300 hover:text-red-600 dark:hover:text-red-400 hover:bg-gray-50 dark:hover:bg-gray-700"
-              >
-                Deleted Posts
-              </a>
+              
             </div>
           </div>
         )}
